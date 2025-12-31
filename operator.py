481d97a711f00c5c9d47a086f0c9a468f6169881
Paras Sonagara <kumarparas5678@gmail.com>
2025-12-31 08:23:11 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,57 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#1.assignment operator
+x=10
+print(x)
+10
+
+#2.unary minus
+a=5
+print(-a)
+-5
+
+#3.relational
+p=10
+q=5
+print(p>q)
+True
+print(p==q)
+False
+
+#4.logical
+x=True
+y=False
+print(x and y)
+False
+print(x or y)
+True
+print(not x)
+False
+
+#5.boolean
+is_pass=True
+print(is_pass)
+True
+
+#bitwise
+a=5
+b=3
+print(a & b)
+1
+print(a | b)
+7
+
+#7.membership
+numbers=[1,2,3,4,5]
+print(2 in numbers)
+True
+print(5 not in numbers)
+False
+
+#8.identity
+x=10
+y=10
+print(x is y)
+True
+print(x is not y)
+False
