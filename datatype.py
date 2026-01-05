2e4bf06b4548086df00a76bab6f85ab979aef688
Paras Sonagara <kumarparas5678@gmail.com>
2026-01-05 11:15:30 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,46 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#1.String
+x="hello world"
+print(x)
+hello world
+
+#2.Int
+a=20
+print(a)
+20
+
+#3.Boolean
+x=True
+print(x)
+True
+
+#4.Tuple
+x=("apple","banana","cherry")
+print(x)
+('apple', 'banana', 'cherry')
+
+#5.List
+b=["apple","banana","cherry"]
+print(b)
+['apple', 'banana', 'cherry']
+
+#6.Dic
+x={"name":"john","age":36}
+print(x)
+{'name': 'john', 'age': 36}
+
+#7.Complex
+c=1j
+print(c)
+1j
+
+#8.Float
+x=20.3
+print(x)
+20.3
+
+#9.None
+x=None
+print(x)
+None
