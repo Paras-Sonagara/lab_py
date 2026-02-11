0cd74dd13ca3c30d048e54fa8a0d426430beefb4
Paras Sonagara <kumarparas5678@gmail.com>
2026-02-11 07:46:47 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,30 @@
+x="hello world"
+print(x)
+
+#1.Int
+age = int(input("Enter Your Age :"))
+print(age)
+
+number = int(input("Enter Your Number :"))
+print(number)
+
+#2.String
+name = str(input("Enter Your Name :"))
+print(name)
+
+address = str(input("Enter Your Address :"))
+print(address)
+
+#3.Float
+point = float(input("Enter Your point value :"))
+print(point)
+
+float = float(input("Enter Your Address :"))
+print(float)
+
+#4.Boolean
+boolean = bool(input("Enter Your Name :"))
+print(boolean)
+
+x = bool(input("Enter Your Address :"))
+print(x)
