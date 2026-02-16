1c9b760e155e4570d78f67628c93db607a33492c
Paras Sonagara <kumarparas5678@gmail.com>
2026-02-16 11:33:00 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,41 @@
+#1.TASK
+for i in range(1,11):
+    print(i)
+    
+#2.TASK
+for i in range(1,21):
+    if i%2==0:
+        print(i)
+        
+#3.TASK
+for i in range(1,16):
+    if i%2!=0:
+        print(i)
+        
+#4.TASK
+for i in range(1,11):
+    print("5x",i,"=",5*i)
+    
+#5.TASK
+name="Atmiya"
+for letter in name:
+    print(letter)
+    
+#6.TASK
+total=0
+for i in range(1,6):
+    total=total+i
+    print("Sum is :",total)
+    
+#7.TASK
+numbers=[10,20,30,40]
+for n in numbers:
+    print(n)
+    
+#8.TASK
+for i in range(1,6):
+    print(i)
+    
+#9.TASK
+for i in range(1,3):
+    print("Hello")
