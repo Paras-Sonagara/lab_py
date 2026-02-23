16f4455ae625ccd0877e016285778ae77fab0d55
Paras Sonagara <kumarparas5678@gmail.com>
2026-02-23 11:16:22 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,27 @@
+#1.
+i = 1             
+while i <= 5:
+    print(i)
+    i = i + 1
+    
+#2.
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+    
+#3.
+i = 1
+s= 0
+while i <= 10:
+    s = s + i
+    i = i + 1
+    print("Sum =",s)
+    
+#4.
+num=int(input("Enter Number :"))
+i = 1
+while i <= 10:
+    print(num,"x",i,"=",num*i)
+    i = i + 1
+    
